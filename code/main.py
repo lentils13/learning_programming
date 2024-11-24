feaae0a2d156dc6b6569951f1f108dda80837738
lentils13 <proskurinpg1@mail.com>
2024-11-24 16:32:42 +0300

    prime number 1.0.0

--- code/main.py
+++ code/main.py
@@ -1,6 +1,26 @@
-l = [20, 23, 57, 18, -18, 3, 228]
+def is_prime(number):
+    if number == 1:
+        return False
+    if number == 2:
+        return True
+    if number % 2 == 0:
+        return False
+    t = 3
+    for t in range(3, number // 2, 2):
+        if number % t == 0:
+            return False
+    return True
+
+# def is_even(number):
+#     return number % 2 == 0 
+
+# print(is_prime(1))
+# print(is_prime(8))
+# print(is_prime(13))
+# print(is_prime(2))
+l = [6363107417]
 k = 0
-for a in l:
-    if a > 0 and a % 2:
-        k += 1
+for f in l:
+     if is_prime(f):
+          k += 1
 print(k)
